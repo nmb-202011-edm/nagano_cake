23975cbee54aaf63c1d5901b4fcfb7c3418e32fb
EC2 Default User <ec2-user@ip-172-31-20-189.ec2.internal>
2020-12-18 09:13:19 +0000

    [add] item_index

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  
   attachment :image
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
 end
