712563827b93d9431836daff21cb646e7b5501e5
EC2 Default User <ec2-user@ip-172-31-20-189.ec2.internal>
2020-12-18 05:11:10 +0000

    12/18

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,7 +22,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image, :genre_id)
+    params.require(:item).permit(:name, :introduction, :price, :image, :genre_id, :is_active)
   end
   
 end
