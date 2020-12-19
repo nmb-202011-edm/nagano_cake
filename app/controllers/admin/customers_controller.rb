7066c045686331c6b51fc72e0523a3a46707ef1c
Yuichi-K-11 <banaichi1@gmail.com>
2020-12-19 09:42:06 +0000

    [add]admin_c_update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,17 +1,17 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
   if @customer.update(customer_params)
@@ -20,12 +20,12 @@ class Admin::CustomersController < ApplicationController
   else
   	render "edit"
   end
-  
+
   end
 
   private
     def customer_params
-      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :_name_kana, :email, :postal_code, :address, :telephone_numberphone_numbar)
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
     end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,17 @@
 class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
 
-   # 新規登録後のリダイレクト先をマイページへ
+
   def after_sign_in_path_for(resource)
     if current_admin
       flash[:notice] = "ログイン"
-      admin_path  #　指定したいパスに変更
-    # else
-    #   flash[:notice] = "新規登録完了しました。"
-    #   new_profile_path  #　指定したいパスに変更
+      admin_path
     end
   end
-
-  # def after_sign_out_path_for(resource)
-  #   if current_admin
-  #     flash[:notice] = "ログアウト"
-  #     admin_path  #　指定したいパスに変更
-  #   end
-  # end
-
 
  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:id, :last_name, :first_name, :last_name_kana, :_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:id, :last_name, :first_name, :last_name_kana, :_name_kana, :email,:postal_code, :address, :telephone_number, :is_deleted])
   end
 end
