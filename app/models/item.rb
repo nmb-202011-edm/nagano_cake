b0e461dff30c63231709983a2e5922bd8672cd60
Yuichi-K-11 <banaichi1@gmail.com>
2020-12-20 15:34:32 +0000

    [add]cart_destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  
+  has_many :cart_items, dependent: :destroy
+
   attachment :image
-  
+
   validates :image, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
