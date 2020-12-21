1d0bd4846346e2aa3bcdc9ff23c62f1649625917
Yuichi-K-11 <banaichi1@gmail.com>
2020-12-21 12:25:34 +0000

    [finish]cart_c

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,38 +4,40 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = CartItem.all
-    @customer = Customer.find(current_customer.id)
+    @cart_items = current_customer.cart_items
+    
+    
+
   end
 
   def create
-    @cart_item = current_customer.cart_items.new(cart_item_params)
-
+    @cart_item = CartItem.new(cart_item_params)
     # カート内に同商品が重複しないよう商品の数量を合算
-    @update_cart_item =  CartItem.find_by(item: @cart_item.item)
-    if @update_cart_item.present? && @cart_item.valid?
-        @cart_item.amount += @update_cart_item.amount
-        @update_cart_item.destroy
-    end
 
-    if @cart_item.save
-      flash[:notice] = "#{@cart_item.item.name}をカートに追加しました"
-      redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: @cart_item.item_id).present?
+      @old_cart_item =  current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+      @old_cart_item.amount += @cart_item.amount
+      @old_cart_item.save
+      # flash[:notice] = "#{@cart_item.item.name}をカートに追加しました"
+
     else
-      @item = Item.find(params[:cart_item][:item_id])
-      @cart_item = CartItem.new
-      flash[:alert] = "個数を選択してください"
-      render ("customer/items/show")
+      @cart_item.save
+    # else
+    #   @item = Item.find(params[:cart_item][:item_id])
+    #   @cart_item = CartItem.new
+    #   # flash[:alert] = "個数を選択してください"
+    #   render ("customer/items/show")
     end
+    redirect_to cart_items_path
   end
 
   def update
-    @cart_item.update(amount: params[:cart_item][:amount].to_i)
-    flash[:success] = "#{@cart_item.item.name}の数量を変更しました"
-    @price = sub_price(@cart_item).to_s(:delimited)
-    @cart_items = current_cart
-    @total = total_price(@cart_items).to_s(:delimited)
-    @customer = Customer.find(current_customer.id)
+
+    @cart_item = CartItem.find(params[:id])
+		@cart_item.update(cart_item_params)
+    @cart_items = current_customer.cart_items
+
+    # flash[:success] = "#{@cart_item.item.name}の数量を変更しました"
     render :index
   end
 
@@ -62,6 +64,6 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :item_id)
+    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
   end
 end
