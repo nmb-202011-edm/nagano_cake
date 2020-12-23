70c7ac9d6e4fdeffb1b674f4b70d05dc67f0844d
“dam-san” <ryococokuronene@gmail.com>
2020-12-23 05:43:43 +0000

    #

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,8 @@
 class Admin::HomesController < ApplicationController
-    
+
     def top
-        
+        # orders=Order.where(status: "waiting")
+        # @sum=orders.size
     end
-    
+
 end
